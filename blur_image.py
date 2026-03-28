a20987b1d798f837ad5f846098f8af87c61415a3
David <kingdavidcreative201@gmail.com>
2026-03-28 22:38:13 +0300

    Initial commit

---
+++ blur_image.py
@@ -0,0 +1,12 @@
+from functools import reduce
+from image_lib import Image
+   
+# add your blur function here
+
+def main():
+    my_image = Image("smokey.gif")
+    # call the blur function here
+    # save the blurred image to a PNG file here
+
+if __name__ == "__main__":
+    main()
